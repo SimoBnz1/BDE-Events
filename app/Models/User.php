51b85f745c0037757a6fb698a02a30a15de466f1
mohamed_bnz <simobnz81@gmail.com>
2026-07-22 14:50:51 +0100

    Ajouter le code du model User avec ses relations

---
+++ app/Models/User.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+}
