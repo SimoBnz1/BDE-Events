625e2321bb5f1f1ae79fd8a9d6bb4b9aa0202cf8
mohamed_bnz <simobnz81@gmail.com>
2026-07-25 13:36:08 +0100

    ajouter le route pour afficher le formulair de modification

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ use App\Http\Controllers\ReservationController;
 Route::get('/', [EventController::class, 'index'])->name('index');
 Route::get('/login', [AuthController::class, 'showLogin'])->name('showLogin');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/events/category/{category}', [EventController::class,'filter'])
-->name('events.category');
+Route::get('/events/category/{category}', [EventController::class, 'filter'])
+    ->name('events.category');
 
 
 Route::middleware(['auth'])->group(function () {
@@ -21,17 +21,21 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/admin/reservations', [HomeController::class, 'reservations'])->name('admin.reservations');
         Route::get('/formEvenment', [EventController::class, 'formEvenment'])->name('creatEvenment');
         Route::post('/addEvenment', [EventController::class, 'store'])->name('AddEvents');
+
+        Route::get('/admin/events/{event}/edit', [EventController::class, 'edit'])
+            ->name('events.edit');
+
     });
 
 
- 
-        Route::get('/student/dashboard', [HomeController::class, 'dashboardStudent'])->name('student.dashboard');
-        Route::get('/student/events', [HomeController::class, 'eventsStudent'])->name('student.events');
-        Route::get('/Reservation', [ReservationController::class, 'index'])->name('student.reservations');
-        Route::post('/Reservation/{event}', [ReservationController::class, 'store'])->name('Reservations');
-        Route::delete('/student/reservation/{reservation}', [ReservationController::class, 'destroy'])->name('student.reservation.destroy');
-        Route::get('/student/ticket/{reservation}', [ReservationController::class, 'showTicket'])->name('student.ticket.show');
-   
+
+    Route::get('/student/dashboard', [HomeController::class, 'dashboardStudent'])->name('student.dashboard');
+    Route::get('/student/events', [HomeController::class, 'eventsStudent'])->name('student.events');
+    Route::get('/Reservation', [ReservationController::class, 'index'])->name('student.reservations');
+    Route::post('/Reservation/{event}', [ReservationController::class, 'store'])->name('Reservations');
+    Route::delete('/student/reservation/{reservation}', [ReservationController::class, 'destroy'])->name('student.reservation.destroy');
+    Route::get('/student/ticket/{reservation}', [ReservationController::class, 'showTicket'])->name('student.ticket.show');
+
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
